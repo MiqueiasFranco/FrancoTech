8896bd85979b0e67b110f10509bc02462dbf1d9e
unknown <miqueiassantos1552@gmail.com>
2025-07-08 20:43:12 -0300

    criacao da area do carrinho

--- www/js/routes.js
+++ www/js/routes.js
@@ -20,11 +20,13 @@ var app = new Framework7({
     {
       path: '/index/',
       url: 'index.html',
-      animate: false,
+      options: {
+        transition: 'f7-cover-v',
+      },
 	  on: {
 		pageBeforeIn: function (event, page) {
 		// fazer algo antes da página ser exibida
-        //$("#menu-principal").hide('fast')
+          $("#menu-principal").show('fast')
 
 		},
 		pageAfterIn: function (event, page) {
@@ -32,7 +34,8 @@ var app = new Framework7({
 		},
 		pageInit: function (event, page) {
 		// fazer algo quando a página for inicializada
-      app.views.main.router.navigate('/detalhes/');
+
+      app.views.main.router.navigate('/carrinho/');
 
       $.getScript('js/index.js');
       var swiper = new Swiper(".mySwiper", {
@@ -155,7 +158,30 @@ var app = new Framework7({
 	  on: {
         pageBeforeIn: function (event, page) {
         // fazer algo antes da página ser exibida
-           // $("#menu-principal").hide('fast')
+          $("#menu-principal").hide('fast')
+
+        },
+        pageAfterIn: function (event, page) {
+        // fazer algo depois da página ser exibida
+        },
+        pageInit: function (event, page) {
+        // fazer algo quando a página for inicializada
+        },
+        pageBeforeRemove: function (event, page) {
+        // fazer algo antes da página ser removida do DOM
+        },
+	  }
+    },
+    {
+      path: '/carrinho/',
+      url: 'carrinho.html',
+      options: {
+        transition: 'f7-push',
+      },
+	  on: {
+        pageBeforeIn: function (event, page) {
+        // fazer algo antes da página ser exibida
+          $("#menu-principal").hide('fast')
 
         },
         pageAfterIn: function (event, page) {
